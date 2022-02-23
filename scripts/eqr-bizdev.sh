c2213880770bd69e08627ad67b48919f9f0c6035
Zane Selvans <zane.selvans@catalyst.coop>
2022-02-23 14:01:53 -0600

    Use GITHUB_WORKSPACE in path to EQR BizDev outputs

--- scripts/eqr-bizdev.sh
+++ scripts/eqr-bizdev.sh
@@ -2,4 +2,4 @@
 
 TODAY=`date --iso`
 EQR_FTP="ftp://eqrdownload.ferc.gov/DownloadRepositoryProd/Selective/"
-curl --list-only $EQR_FTP >> ../eqr-bizdev/$TODAY-eqr-bizdev.txt
+curl --list-only $EQR_FTP >> $GITHUB_WORKSPACE/eqr-bizdev/$TODAY-eqr-bizdev.txt
