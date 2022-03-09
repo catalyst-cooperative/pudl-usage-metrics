dd4516d0bf6eb95ec7aaf9bea9f17cc479db87cf
bendnorman <bdn29@cornell.edu>
2022-03-09 10:42:02 -0900

    Create general dagster metrics project structure

---
+++ usage_metrics/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="usage_metrics",
+    packages=setuptools.find_packages(exclude=["usage_metrics_tests"]),
+    install_requires=[
+        "dagster==0.14.3",
+        "dagit==0.14.3",
+        "pytest",
+    ],
+)
---
+++ usage_metrics/usage_metrics/__init__.py
@@ -0,0 +1,1 @@
+from .repository import usage_metrics
