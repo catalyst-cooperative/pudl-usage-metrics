936e027f43c61a486c2259cee770db0350ea0c85
bendnorman <bdn29@cornell.edu>
2022-04-20 11:04:08 -0800

    Create db metadata

--- usage_metrics/usage_metrics/resources/sqlite.py
+++ usage_metrics/usage_metrics/resources/sqlite.py
@@ -5,22 +5,25 @@ import pandas as pd
 import sqlalchemy as sa
 from dagster import resource
 
+from usage_metrics.models import usage_metrics_metadata
+
 
 class SQLiteManager:
     """Manage connection with SQLite Database."""
 
-    # the locationo of the db?
-    # the metadata?
     def __init__(self) -> None:
         """Initialize SQLiteManager object."""
-        self.engine = self.get_engine()
+        self.engine = self.setup_db()
 
     @staticmethod
-    def get_engine() -> sa.engine.Engine:
-        """Create a sql alchemy engine for sqlite db."""
+    def setup_db():
+        """Create a sqlite db if it doesn't exist and create table schemas."""
         sqlite_path = Path(__file__).parents[2] / "data/usage_metrics.db"
-        sqlite_path.touch()
-        return sa.create_engine("sqlite:///" + str(sqlite_path))
+        engine = sa.create_engine("sqlite:///" + str(sqlite_path))
+        if not sqlite_path.exists():
+            sqlite_path.touch()
+            usage_metrics_metadata.create_all(engine)
+        return engine
 
     def append_df_to_table(self, df: pd.DataFrame, table_name: str) -> None:
         """Append a dataframe to a table in the db."""
