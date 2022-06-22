8d89da589b30d612243ec5809bf900c55c591d68
bendnorman <bdn29@cornell.edu>
2022-06-22 15:23:54 -0800

    Dagster methods to launch runs instead of running as a subprocess

--- run_data_update.py
+++ run_data_update.py
@@ -1,19 +1,10 @@
 """Temporary script to run scheduled dagster jobs."""
 import datetime
 import logging
-import subprocess  # nosec B404
 
 import coloredlogs
 
 from usage_metrics.jobs.datasette import process_datasette_logs_gcp
-
-CONFIG_TEMPLATE = """
-ops:
-  extract:
-    config:
-      end_date: "{}"
-      start_date: "{}"
-"""
 
 DATASET_JOBS = {"datasette": process_datasette_logs_gcp}
 
@@ -26,56 +17,26 @@ def main():
 
     yesterday = datetime.date.today() - datetime.timedelta(days=1)
 
-    return_codes = []
-
     # Run the jobs
     for _, job in DATASET_JOBS.items():
         partition_set = job.get_partition_set_def()
         most_recent_partition = max(
             partition_set.get_partitions(), key=lambda x: x.value.start
-        ).value
+        )
+        time_window = most_recent_partition.value
 
         # Run the most recent partition if the end_date it yesterday
-        if most_recent_partition.start.date() != yesterday:
+        if time_window.start.date() != yesterday:
             usage_metrics_logger.info(
                 f"No partition for {job.name} yesterday, skipping."
             )
             continue
         usage_metrics_logger.info(
-            f"""Processing partition: ({most_recent_partition.start.date()},
-                {most_recent_partition.end.date()}) for {job.name}."""
+            f"""Processing partition: ({time_window.start.date()},
+                {time_window.end.date()}) for {job.name}."""
         )
 
-        start_date = most_recent_partition.end.date()
-        with open("schedule_run_config.yaml", "w") as f:
-            f.write(CONFIG_TEMPLATE.format(yesterday, start_date))
-
-        return_code = subprocess.run(
-            [
-                "dagster",
-                "job",
-                "execute",
-                "-m",
-                "usage_metrics",
-                "-j",
-                job.name,
-                "-c",
-                "schedule_run_config.yaml",
-            ]
-        ).returncode
-        return_codes.append(return_code)
-
-        if return_code != 0:
-            usage_metrics_logger.error(
-                f"{job.name} job failed with exit code: {return_code}"
-            )
-        else:
-            usage_metrics_logger.info(f"{job.name} job completed successfully.")
-
-    # Exit with any failed exit codes
-    if not all(x == 0 for x in return_codes):
-        usage_metrics_logger.error("One of the dagster jobs failed.")
-        exit(1)
+        job.execute_in_process(partition_key=most_recent_partition.name)
 
 
 if __name__ == "__main__":
