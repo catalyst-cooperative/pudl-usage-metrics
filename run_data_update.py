55545436dda81b04d2fc9b46013b52d79ccf245c
bendnorman <bdn29@cornell.edu>
2022-06-22 15:33:44 -0800

    Change run condition to today instead of yesterday

--- run_data_update.py
+++ run_data_update.py
@@ -15,7 +15,7 @@ def main():
     log_format = "%(asctime)s [%(levelname)8s] %(name)s:%(lineno)s %(message)s"
     coloredlogs.install(fmt=log_format, level="INFO", logger=usage_metrics_logger)
 
-    yesterday = datetime.date.today() - datetime.timedelta(days=1)
+    today = datetime.date.today()
 
     # Run the jobs
     for _, job in DATASET_JOBS.items():
@@ -25,18 +25,18 @@ def main():
         )
         time_window = most_recent_partition.value
 
-        # Run the most recent partition if the end_date it yesterday
-        if time_window.start.date() != yesterday:
+        # Run the most recent partition if the end_date is today
+        if time_window.end.date() == today:
             usage_metrics_logger.info(
-                f"No partition for {job.name} yesterday, skipping."
-            )
-            continue
-        usage_metrics_logger.info(
-            f"""Processing partition: ({time_window.start.date()},
+                f"""Processing partition: ({time_window.start.date()},
                 {time_window.end.date()}) for {job.name}."""
-        )
+            )
 
-        job.execute_in_process(partition_key=most_recent_partition.name)
+            job.execute_in_process(partition_key=most_recent_partition.name)
+        else:
+            usage_metrics_logger.info(
+                f"No scheduled partition for {job.name} yesterday, skipping."
+            )
 
 
 if __name__ == "__main__":
