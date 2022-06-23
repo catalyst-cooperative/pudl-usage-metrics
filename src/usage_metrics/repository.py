58744eb98b9442500ea47e17f4fd1f519273224f
bendnorman <bdn29@cornell.edu>
2022-06-22 17:30:06 -0800

    Load jobs automatically into repository

--- src/usage_metrics/repository.py
+++ src/usage_metrics/repository.py
@@ -1,13 +1,26 @@
 """Usage metrics dagster repository."""
-from dagster import repository
+import importlib
+import pkgutil
+from pathlib import Path
 
-from usage_metrics.jobs.datasette import (
-    process_datasette_logs_gcp,
-    process_datasette_logs_locally,
-)
+from dagster import JobDefinition, repository
 
 
 @repository
 def usage_metrics():
     """Define dagster repository for usage_metrics."""
-    return [process_datasette_logs_locally, process_datasette_logs_gcp]
+    jobs = []
+
+    jobs_subpackage_path = Path(__file__).parent / "jobs"
+    modules = [str(jobs_subpackage_path)]
+
+    for module_info in pkgutil.iter_modules(modules):
+        module = importlib.import_module(f"usage_metrics.jobs.{module_info.name}")
+
+        # Get all of the JobDefinitions
+        for attr_name in dir(module):
+            attr = getattr(module, attr_name)
+            if type(attr) == JobDefinition:
+                jobs.append(attr)
+
+    return jobs
