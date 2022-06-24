00d4a63a35840b6748a4bba1ffaaca21b9982740
bendnorman <bdn29@cornell.edu>
2022-06-23 16:43:19 -0800

    Use UTC for current date

--- run_data_update.py
+++ run_data_update.py
@@ -1,6 +1,6 @@
 """Temporary script to run scheduled dagster jobs."""
-import datetime
 import logging
+from datetime import datetime, timezone
 
 import coloredlogs
 
@@ -13,7 +13,7 @@ def main():
     log_format = "%(asctime)s [%(levelname)8s] %(name)s:%(lineno)s %(message)s"
     coloredlogs.install(fmt=log_format, level="INFO", logger=usage_metrics_logger)
 
-    today = datetime.date.today()
+    today = datetime.now(tz=timezone.utc).date()
 
     # Run the jobs
     for job in gcp_usage_metrics.get_all_jobs():
@@ -22,6 +22,7 @@ def main():
             partition_set.get_partitions(), key=lambda x: x.value.start
         )
         time_window = most_recent_partition.value
+        usage_metrics_logger.info(time_window)
 
         # Run the most recent partition if the end_date is today
         if time_window.end.date() == today:
