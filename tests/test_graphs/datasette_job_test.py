fc2376e09f5229488f873e7d494065fee8bd368a
e-belfer <ella.belfer@catalyst.coop>
2024-08-14 14:35:09 -0400

    Xfail datasette/intake tests

--- tests/test_graphs/datasette_job_test.py
+++ tests/test_graphs/datasette_job_test.py
@@ -1,36 +1,43 @@
 """Test usage metrics dagster jobs."""
 
-
-### TEMP - Disable until Datasette intake is updated
-# def test_datasette_job(datasette_partition_config, sqlite_engine):
-#     """Process a single partition of datassette."""
-#     usage_metrics_metadata.drop_all(sqlite_engine)
-#     result = process_datasette_logs_locally.execute_in_process(
-#         run_config=datasette_partition_config
-#     )
-
-#     assert result.success
-
-#     # Make sure we got the correct number of rows.
-#     with sqlite_engine.connect() as con:
-#         logs = pd.read_sql(
-#             "select insert_id from datasette_request_logs"
-#             " where timestamp < '2022-02-06'",
-#             con,
-#         )
-#     assert len(logs) == 891
+import pandas as pd
+import pytest
+import sqlalchemy as sa
+from usage_metrics.jobs.datasette import process_datasette_logs_locally
+from usage_metrics.models import usage_metrics_metadata
 
 
-# def test_primary_key_failure(datasette_partition_config, sqlite_engine):
-#     """Reprocess the same partition as `test_datasette_job` test for integrity error."""
-#     usage_metrics_metadata.drop_all(sqlite_engine)
-#     result = process_datasette_logs_locally.execute_in_process(
-#         run_config=datasette_partition_config
-#     )
+@pytest.mark.xfail(reason="Xfail until we reconfigure Datasette ETL.")
+def test_datasette_job(datasette_partition_config, sqlite_engine):
+    """Process a single partition of datassette."""
+    usage_metrics_metadata.drop_all(sqlite_engine)
+    result = process_datasette_logs_locally.execute_in_process(
+        run_config=datasette_partition_config
+    )
 
-#     assert result.success
+    assert result.success
 
-#     with pytest.raises(sa.exc.IntegrityError):
-#         _ = process_datasette_logs_locally.execute_in_process(
-#             run_config=datasette_partition_config
-#         )
+    # Make sure we got the correct number of rows.
+    with sqlite_engine.connect() as con:
+        logs = pd.read_sql(
+            "select insert_id from datasette_request_logs"
+            " where timestamp < '2022-02-06'",
+            con,
+        )
+    assert len(logs) == 891
+
+
+@pytest.mark.xfail(reason="Xfail until we reconfigure Datasette ETL.")
+def test_primary_key_failure(datasette_partition_config, sqlite_engine):
+    """Reprocess the same partition as `test_datasette_job` test for integrity error."""
+    usage_metrics_metadata.drop_all(sqlite_engine)
+    result = process_datasette_logs_locally.execute_in_process(
+        run_config=datasette_partition_config
+    )
+
+    assert result.success
+
+    with pytest.raises(sa.exc.IntegrityError):
+        _ = process_datasette_logs_locally.execute_in_process(
+            run_config=datasette_partition_config
+        )
--- tests/test_graphs/intake_job_test.py
+++ tests/test_graphs/intake_job_test.py
@@ -1,19 +1,23 @@
 """Test usage metrics dagster jobs."""
 
+import pandas as pd
+import pytest
+from usage_metrics.jobs.intake import process_intake_logs_locally
 
-### TEMP - Disable until Datasette intake is updated
-# def test_intake_job(sqlite_engine, intake_partition_config):
-#     """Process a single partition of intake logs."""
-#     result = process_intake_logs_locally.execute_in_process(
-#         run_config=intake_partition_config
-#     )
 
-#     assert result.success
+@pytest.mark.xfail(reason="Xfail until we reconfigure datasette ETL.")
+def test_intake_job(sqlite_engine, intake_partition_config):
+    """Process a single partition of intake logs."""
+    result = process_intake_logs_locally.execute_in_process(
+        run_config=intake_partition_config
+    )
 
-#     # Make sure we got the correct number of rows.
-#     with sqlite_engine.connect() as con:
-#         logs = pd.read_sql(
-#             "select insert_id from intake_logs",
-#             con,
-#         )
-#     assert len(logs) == 6
+    assert result.success
+
+    # Make sure we got the correct number of rows.
+    with sqlite_engine.connect() as con:
+        logs = pd.read_sql(
+            "select insert_id from intake_logs",
+            con,
+        )
+    assert len(logs) == 6
