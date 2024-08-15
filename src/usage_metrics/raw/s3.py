79ea5fea35f01ddc68bb245f61efa8b65b3d0969
e-belfer <ella.belfer@catalyst.coop>
2024-08-15 11:26:54 -0400

    Fix tempfile behavior

--- src/usage_metrics/raw/s3.py
+++ src/usage_metrics/raw/s3.py
@@ -15,26 +15,15 @@ from tqdm import tqdm
 
 BUCKET_URI = "pudl-s3-logs.catalyst.coop"
 PATH_EXT = "data/pudl_s3_logs/"
-# if not Path.exists(Path(LOCAL_DIR)):
-#     Path.mkdir(LOCAL_DIR)
 
 
 def download_s3_logs_from_gcs(
-    context: AssetExecutionContext,
-    partition_dates: tuple[str],
+    context: AssetExecutionContext, partition_dates: tuple[str], download_dir: Path
 ) -> list[Path]:
     """Download all logs from GCS bucket.
 
     If the file already exists locally don't download it.
     """
-    # Determine where to save these files
-    download_dir = Path(
-        os.environ.get("DATA_DIR", TemporaryDirectory().name), "pudl_s3_logs/"
-    )
-    if not Path.exists(download_dir):
-        Path.mkdir(download_dir, parents=True)
-    context.log.info(f"Saving S3 logs to {download_dir}.")
-
     bucket = storage.Client().get_bucket(BUCKET_URI)
     blobs = bucket.list_blobs()
     blobs = [blob for blob in blobs if blob.name.startswith(partition_dates)]
@@ -61,13 +50,27 @@ def raw_s3_logs(context: AssetExecutionContext) -> pd.DataFrame:
     week_date_range = pd.date_range(start=week_start_date_str, periods=7, freq="D")
 
     weekly_dfs = []
-    file_paths = download_s3_logs_from_gcs(
-        context,
-        tuple(week_date_range.strftime("%Y-%m-%d")),
-    )  # Get all logs in a day
-    for path in file_paths:
-        try:
-            weekly_dfs.append(pd.read_csv(path, delimiter=" ", header=None))
-        except pd.errors.EmptyDataError:
-            context.log.warnings(f"{path} is an empty file, couldn't read.")
-    return pd.concat(weekly_dfs)
+
+    with TemporaryDirectory() as td:
+        # Determine where to save these files
+        if os.environ.get("DATA_DIR"):
+            download_dir = Path(os.environ.get("DATA_DIR"), "pudl_s3_logs/")
+            if not Path.exists(download_dir):
+                Path.mkdir(download_dir)
+        else:
+            download_dir = td
+        context.log.info(f"Saving S3 logs to {download_dir}.")
+
+        # Get all logs in a week
+        file_paths = download_s3_logs_from_gcs(
+            context=context,
+            partition_dates=tuple(week_date_range.strftime("%Y-%m-%d")),
+            download_dir=download_dir,
+        )
+
+        for path in file_paths:
+            try:
+                weekly_dfs.append(pd.read_csv(path, delimiter=" ", header=None))
+            except pd.errors.EmptyDataError:
+                context.log.warnings(f"{path} is an empty file, couldn't read.")
+        return pd.concat(weekly_dfs)
