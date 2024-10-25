a8473d4d5131e727fcd57dd0173db5966ef57ca9
e-belfer <ella.belfer@catalyst.coop>
2024-10-25 16:41:16 -0400

    Update cloud SQL notebook and fix migration again

--- migrations/versions/f5dbba621bb1_add_out_zenodo_logs.py
+++ migrations/versions/f5dbba621bb1_add_out_zenodo_logs.py
@@ -1,7 +1,7 @@
 """Add out_zenodo_logs
 
 Revision ID: f5dbba621bb1
-Revises: 324a4c7b342c
+Revises: b4fee22b4a4d
 Create Date: 2024-10-25 13:30:27.163055
 
 """
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'f5dbba621bb1'
-down_revision: Union[str, None] = '324a4c7b342c'
+down_revision: Union[str, None] = 'b4fee22b4a4d'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
