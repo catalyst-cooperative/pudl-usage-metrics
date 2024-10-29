c081950f25a7c59231d70435dfbf87805753311e
e-belfer <ella.belfer@catalyst.coop>
2024-10-29 12:23:44 -0400

    Write core_zenodo_logs to DB

--- src/usage_metrics/core/zenodo.py
+++ src/usage_metrics/core/zenodo.py
@@ -12,7 +12,7 @@ from dagster import (
 
 @asset(
     partitions_def=WeeklyPartitionsDefinition(start_date="2023-08-16"),
-    # io_manager_key="database_manager",
+    io_manager_key="database_manager",
     tags={"source": "zenodo"},
 )
 def core_zenodo_logs(
