b98fb5e5e0d84e82b091f70176c29dc9b9be7d12
Christina Gosnell <cgosnell@catalyst.coop>
2025-06-16 15:40:02 -0400

    revert to giving alembic a class instead of an io_manager fiiiiine and adding alembic to gha

--- tests/unit/resources_test.py
+++ tests/unit/resources_test.py
@@ -1,15 +1,17 @@
 """Unit tests for dagster resources."""
 
+from pathlib import Path
+
 import pandas as pd
 import pytest
 from dagster import build_output_context
 
-from usage_metrics.resources.sqlite import sqlite_manager
+from usage_metrics.resources.sqlite import SQLiteIOManager
 
 
-def test_missing_schema() -> None:
+def test_missing_schema(sqlite_db_path) -> None:
     """Test missing schema assertion."""
-    sq = sqlite_manager()
+    sq = SQLiteIOManager(db_path=Path(sqlite_db_path))
     context = build_output_context(partition_key="1980-01-01")
     with pytest.raises(AssertionError):
         sq.append_df_to_table(context, pd.DataFrame(), "fake_name")
